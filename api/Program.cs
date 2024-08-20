ed2a0448c76a3ebcb30f45986f44c3aece38694d
Rayssa <guimaraesr305@gmail.com>
2024-08-20 07:33:28 -0300

    first commit

---
+++ api/Program.cs
@@ -0,0 +1,21 @@
+//control things, like dependecies, provide services
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build(); 
+
+//configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
+
