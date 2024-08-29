c9012e47d8ede4f252e4ccc1e1e276334c0393ed
Rayssa <guimaraesr305@gmail.com>
2024-08-29 08:35:43 -0300

    add newUserDto

---
+++ api/Dtos/Account/NewUserDto.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Dtos.Account
+{
+    public class NewUserDto
+    {
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Token { get; set; }
+    }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -3,6 +3,7 @@ using api.Data;
 using api.Interfaces;
 using api.Models;
 using api.Repository;
+using api.Service;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -60,6 +61,7 @@ builder.Services.AddAuthentication(options => {
 
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<ITokenService, TokenService>();
 
 var app = builder.Build(); 
 
